e0d98576302c55857bb113f506e1caa78e7a3a71
David Castagneto A <76570078+davidcastagnetoa@users.noreply.github.com>
2023-04-09 00:01:37 +0000

    cameras side menu 2

--- english_guide/src/screens/CamerasScreen.jsx
+++ english_guide/src/screens/CamerasScreen.jsx
@@ -39,7 +39,7 @@ const CamerasScreen = () => {
     <div className="w-full flex flex-row h-full font-titillium">
       {/* Left Side Options banner */}
       <div className="hscreen font-titillium">
-        <div className="rounded-md dark:bg-black py-0 px-2 flex flex-col w-max h-full items-center text-black dark:text-white justify-start mr-3">
+        <div className="pt-[3rem] rounded-md dark:bg-black py-0 px-2 flex flex-col w-max h-full items-center text-black dark:text-white justify-start mr-3">
           <h4 className="my-6 font-bold">
             {language === "english"
               ? "Choose a camera model"
